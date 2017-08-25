60897f7713784a0ba5f11be0b452d8997f9cd15d
Brett Henderson <bretth@m2north.com>
2017-08-25 18:07:44 +0200

    add copy-webpack-plugin to copy assets folder to the build folder

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -1,11 +1,13 @@
 const path = require( 'path' );
 const webpack = require( 'webpack' );
+const CopyWebpackPlugin = require( "copy-webpack-plugin" );
 const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
 
 module.exports = function () {
 
 	return {
-		entry:   './src/main.js',
+		context: path.resolve(),
+		entry: path.resolve( "src", "main.js" ),
 		resolve: {
 			modules: [
 				path.resolve( "src" ),
@@ -13,7 +15,7 @@ module.exports = function () {
 			]
 		},
 		output:  {
-			path: path.resolve( '..', 'build' ),
+			path: path.resolve( 'build' ),
 			filename:          '[name].js',
 			publicPath:        '/',
 			sourceMapFilename: '[name].map'
@@ -28,17 +30,27 @@ module.exports = function () {
 				{
 					test: /(\.txt|\.onsong)$/,
 					use:  'raw-loader'
-				},
+				}/*,
 				{
 					test:    /\.(jpe?g|png|gif|svg)$/i,
 					loaders: [
 						'url?limit=8192',
 						'img-loader'
 					]
-				}
+				}*/
 			]
 		},
 		plugins: [
+			new CopyWebpackPlugin( [
+				{
+					from: path.resolve( "src", "assets", "favicon.ico" ),
+					to:   "favicon.ico"
+				},
+				{
+					from: path.resolve( "src", "assets" ),
+					to:   "assets"
+				}
+			] ),
 			new HtmlWebpackPlugin( {
 				chunkSortMode: 'dependency',
 				filename:      'index.html',
