6926c2bc300dd729fd7b7713616ca7bf6616d2f0
Brett Henderson <bretth@m2north.com>
2017-09-05 17:35:13 +0200

    add set list skeleton

---
+++ src/app/common/SetList.js
@@ -0,0 +1,63 @@
+/*
+{
+    "_id": "set/justin",
+    "type": "set",
+    "sets": [
+        {
+            "title": "Week 1",
+            "songs": [
+                {
+                    "_id": "1231-lksdhaf",
+                    "_rev": "??",
+                    "key": "A"
+                },
+                 {
+                    "_id": "alsd-13490842",
+                    "_rev": "??",
+                    "key": "E"
+                },
+                 {
+                    "_id": "340-2834ljasd",
+                    "_rev": "??",
+                    "key": "G#"
+                }
+            ]
+        },
+        {
+            "title": "Week 2",
+            "songs": [
+                {
+                    "_id": "1231-lksdhaf",
+                    "_rev": "??",
+                    "key": "A"
+                },
+                 {
+                    "_id": "alsd-13490842",
+                    "_rev": "??",
+                    "key": "E"
+                }
+            ]
+        },
+        {
+            "title": "Week 3",
+            "songs": [
+                 {
+                    "_id": "1231-lksdhaf",
+                    "_rev": "??",
+                    "key": "A"
+                }
+            ]
+        }
+    ]
+}
+*/
+
+class SetList extends PreactComponent {
+	render() {
+		return (
+			<div></div>
+		);
+	}
+}
+
+export default SetList;
