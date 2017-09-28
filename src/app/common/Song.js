66731414723b528b03bda56848c0d303f5493a9c
Brett Henderson <bretth@m2north.com>
2017-09-28 15:48:06 +0200

    change song keys on sets

--- src/app/common/Song.js
+++ src/app/common/Song.js
@@ -1,4 +1,5 @@
 import Parser from '../parsers/song-parser.js';
+import transposeChord from './transpose-chord';
 
 class Song {
 	_id = null;
@@ -62,89 +63,3 @@ export default Song;
 
 //--------------------------------------------------------------------------------
 
-//credit: https://codepen.io/Grilly86/pen/rwRYYM
-
-function transposeChord( chord, transpose ) {
-
-	const octave = [
-		"A",
-		"Bb",
-		"B",
-		"C",
-		"C#",
-		"D",
-		"Eb",
-		"E",
-		"F",
-		"F#",
-		"G",
-		"G#"
-	];
-
-	// Make chords uppercase
-	let newChord = chord.toUpperCase()
-		.replace( /([A-G])([BM])/g, (m, p1, p2) => p1 + p2.toLowerCase() );
-
-	let mod = "";
-	let isMinor = /[A-G]m/.test( newChord );
-
-	if ( newChord.length > 1 ) {
-
-		mod = newChord.substr( 1, 1 );
-		newChord = newChord.substr( 0, 1 );
-
-	}
-
-	let chordNr = parseInt( octave.indexOf( newChord ) );
-
-	if ( chordNr >= 0 ) {
-		chordNr += parseInt( transpose );
-
-		if ( mod === "#" ) {
-			chordNr += 1;
-		}
-		if ( mod === "b" ) {
-			chordNr -= 1;
-		}
-
-		if ( chordNr < 0 ) {
-			chordNr = chordNr + 12;
-		}
-		if ( chordNr >= 12 ) {
-			chordNr = chordNr % 12;
-		}
-	}
-
-	if ( octave[ chordNr ] === undefined ) {
-
-		console.warn( "ERROR octave does not contain chordNr", chordNr, "chord",
-			newChord );
-		return '';
-
-	} else {
-
-		return octave[ chordNr ] + (isMinor ? 'm' : '');
-
-	}
-
-}
-
-// Test transposing.
-if ( module.hot ) {
-
-	const tests = [
-		[ transposeChord( "A", 1 ), "Bb" ],
-		[ transposeChord( "A", -1 ), "G#" ],
-		[ transposeChord( "a", 1 ), "Bb" ],
-		[ transposeChord( "a", -1 ), "G#" ],
-		[ transposeChord( "bb", 1 ), "B" ],
-		[ transposeChord( "bm", 1 ), "Cm" ],
-	];
-
-	tests.forEach( (test, i) => {
-
-		console.assert( test[ 0 ] === test[ 1 ], i + " - expected: " + test[ 1 ] + ", actual: " + test[ 0 ] );
-
-	} );
-
-}
