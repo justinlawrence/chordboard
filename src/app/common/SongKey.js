51210e754cdae06e27b71425b5bad73d88e279b6
Justin Lawrence <justinl@m2north.com>
2017-11-02 17:10:28 +0000

    added select dropdown to songkey

--- src/app/common/SongKey.js
+++ src/app/common/SongKey.js
@@ -1,8 +1,37 @@
 import './song-key.scss';
 
+/*
+
+TODO: brett please could you add a selected element so that the correct key is shown here. thanks!
+*/
 const SongKey = ({ value }) => (
-	<div className="song-key">
-		{value}
+
+	<div class="control is-primary">
+		<div class="select">
+			<select>
+				<option value = "Ab">Ab</option>
+				<option value = "A">A</option>
+				<option value = "A#">A#</option>
+				<option value = "Bb">Bb</option>
+				<option value = "B">B</option>
+				<option value = "C">C</option>
+				<option value = "C#">C#</option>
+				<option value = "Db">Db</option>
+				<option value = "D">D</option>
+				<option value = "D#">D#</option>
+				<option value = "Eb">Eb</option>
+				<option value = "E">E</option>
+				<option value = "F">F</option>
+				<option value = "F#">F#</option>
+				<option value = "Gb">Gb</option>
+				<option value = "G">G</option>
+				<option value = "G#">G#</option>
+			</select>
+		</div>
+
+		<div className="song-key">
+			{value}
+		</div>
 	</div>
 );
 
