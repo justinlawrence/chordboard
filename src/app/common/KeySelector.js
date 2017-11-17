bbc47e7d6f97facf764acf80930e08be3433da48
Justin Lawrence <justinl@m2north.com>
2017-11-17 17:54:03 +0000

    looped the keys in the key dropdown to make it easier to find keys by scrolling down

--- src/app/common/KeySelector.js
+++ src/app/common/KeySelector.js
@@ -14,6 +14,18 @@ class KeySelector extends PreactComponent {
 			<div className="control is-primary">
 				<div className="select">
 					<select onChange={this.handleChange} value={value}>
+						<option value="C">C</option>
+						<option value="C#">C#</option>
+						<option value="Db">Db</option>
+						<option value="D">D</option>
+						<option value="D#">D#</option>
+						<option value="Eb">Eb</option>
+						<option value="E">E</option>
+						<option value="F">F</option>
+						<option value="F#">F#</option>
+						<option value="Gb">Gb</option>
+						<option value="G">G</option>
+						<option value="G#">G#</option>
 						<option value="Ab">Ab</option>
 						<option value="A">A</option>
 						<option value="A#">A#</option>
@@ -31,6 +43,12 @@ class KeySelector extends PreactComponent {
 						<option value="Gb">Gb</option>
 						<option value="G">G</option>
 						<option value="G#">G#</option>
+						<option value="Ab">Ab</option>
+						<option value="A">A</option>
+						<option value="A#">A#</option>
+						<option value="Bb">Bb</option>
+						<option value="B">B</option>
+						<option value="C">C</option>
 					</select>
 				</div>
 			</div>
