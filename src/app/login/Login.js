0c05e8a9041f56b932d4708040e9a33c4ac6b20b
Justin Lawrence <justinl@m2north.com>
2018-07-19 17:56:42 +0100

    login: add text fields for manual login

--- src/app/login/Login.js
+++ src/app/login/Login.js
@@ -3,25 +3,22 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import cx from 'classnames';
-import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
 import GoogleLogin from 'react-google-login';
 import Grid from '@material-ui/core/Grid';
 import Hero from '../../components/Hero';
 import { Link } from 'react-router-dom';
 import Paper from '@material-ui/core/Paper';
 import { setCurrentUser } from 'actions';
+import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
 import chordboardLogo from '../../assets/chordboard-logo-long.png';
 
-import {
-	Google as GoogleIcon,
-	Facebook as FacebookIcon
-} from 'mdi-material-ui';
+import { Google as GoogleIcon, Facebook as FacebookIcon } from 'mdi-material-ui';
 
-
-const styles = theme => ( {
+const styles = theme => ({
 	root: {
 		backgroundColor: theme.heroBackgroundColor,
 		display: 'flex',
@@ -30,11 +27,11 @@ const styles = theme => ( {
 	container: {
 		flexGrow: 1
 	},
-	form: theme.mixins.gutters( {
+	form: theme.mixins.gutters({
 		paddingBottom: theme.spacing.unit * 8,
 		paddingTop: theme.spacing.unit * 2,
 		width: theme.spacing.unit * theme.spacing.unit * 5
-	} ),
+	}),
 	formFooter: {
 		marginTop: theme.spacing.unit * 2
 	},
@@ -61,84 +58,80 @@ const styles = theme => ( {
 	addPaddingTop: {
 		paddingTop: theme.spacing.unit * 4
 	}
-} );
+});
 
 class Login extends Component {
 	state = {
-		id: '',
+		email: '',
 		name: ''
 	};
 
-	handleEmailInput = event => this.setState( { id: event.target.value } );
-	handleNameInput = event => this.setState( { name: event.target.value } );
+	handleEmailInput = event => this.setState({ email: event.target.value });
+	handleNameInput = event => this.setState({ name: event.target.value });
 
 	handleLogin = event => {
-
 		event.preventDefault();
 
 		const user = {
-			id: this.state.id,
+			id: this.state.email,
 			name: this.state.name
 		};
 
-		localStorage.setItem( 'user', JSON.stringify( user ) );
-		this.props.setCurrentUser( user );
+		localStorage.setItem('user', JSON.stringify(user));
+		this.props.setCurrentUser(user);
 
-		if ( this.props.history ) {
-			this.props.history.push( { pathname: '/sets' } );
+		if (this.props.history) {
+			this.props.history.push({ pathname: '/sets' });
 		}
-
 	};
 
 	responseFacebook = response => {
-		if ( response && response.userID ) {
+		if (response && response.userID) {
 			const user = {
 				id: response.userID,
 				name: response.name
 			};
 
-			this.props.setCurrentUser( user );
+			this.props.setCurrentUser(user);
 			try {
-				localStorage.setItem( 'user', JSON.stringify( user ) );
-				localStorage.setItem( 'loginFrom', 'facebook' );
-
-			} catch ( err ) {
-				console.error( 'Could not set `user` in localStorage' );
+				localStorage.setItem('user', JSON.stringify(user));
+				localStorage.setItem('loginFrom', 'facebook');
+			} catch (err) {
+				console.error('Could not set `user` in localStorage');
 			}
 
-			console.log( 'Login.responseFacebook', this.props.history );
+			console.log('Login.responseFacebook', this.props.history);
 
-			if ( this.props.history ) {
-				this.props.history.push( { pathname: '/sets' } );
+			if (this.props.history) {
+				this.props.history.push({ pathname: '/sets' });
 			}
 		}
 	};
 
 	responseGoogle = response => {
-		if ( response && response.userID ) {
+		if (response && response.userID) {
 			const user = {
 				id: response.userID,
 				name: response.name
 			};
 
-			this.props.setCurrentUser( user );
+			this.props.setCurrentUser(user);
 			try {
-				localStorage.setItem( 'user', JSON.stringify( user ) );
-				localStorage.setItem( 'loginFrom', 'google' );
-			} catch ( err ) {
-				console.error( 'Could not set `user` in localStorage' );
+				localStorage.setItem('user', JSON.stringify(user));
+				localStorage.setItem('loginFrom', 'google');
+			} catch (err) {
+				console.error('Could not set `user` in localStorage');
 			}
 
-			console.log( 'Login.responseGoogle', this.props.history );
+			console.log('Login.responseGoogle', this.props.history);
 
-			if ( this.props.history ) {
-				this.props.history.push( { pathname: '/sets' } );
+			if (this.props.history) {
+				this.props.history.push({ pathname: '/sets' });
 			}
 		}
 	};
 
 	render() {
-
 		const props = this.props;
 		const { classes } = this.props;
 
@@ -148,37 +141,59 @@ class Login extends Component {
 			<div className={classes.root}>
 				<Grid container className={classes.container} alignItems="center" justify="center">
 					<form onSubmit={this.handleLogin}>
-
 						<Paper className={classes.form} elevation={10}>
+							<Grid container alignItems="center" direction="column">
+								<img
+									src={chordboardLogo}
+									width="200px"
+									className={cx(classes.addPaddingTop, classes.addPaddingBottom)}
+								/>
 
-							<Grid container alignItems="center"
-							      direction="column">
-
-								<img src={chordboardLogo} width="200px"
-								     className={cx(
-									     classes.addPaddingTop,
-									     classes.addPaddingBottom
-								     )}/>
-
-								<Typography variant="title"
-								            className={classes.addPaddingBottom}>
+								<Typography variant="title" className={classes.addPaddingBottom}>
 									Please Log In
 								</Typography>
 
-								{/* look up mui buttonBase  look up styling on google/fb */}
 								{/* de-styled according to "facebook button without styling" https://www.npmjs.com/package/react-facebook-login */}
+
+								<TextField
+									id="email"
+									label="Email"
+									className={classes.textField}
+									fullWidth
+									value={this.email}
+									onChange={this.handleEmailInput}
+									margin="normal"
+								/>
+
+								<TextField
+									id="name"
+									label="Full Name"
+									className={classes.textField}
+									fullWidth
+									value={this.name}
+									onChange={this.handleNameInput}
+									margin="normal"
+								/>
+
+								<Button variant="outlined" className={classes.button} type="submit" fullWidth>
+									Login
+								</Button>
+
+								<div className={classes.addPaddingBottom}>
+								</div>
 
 								<FacebookLogin
 									appId="2075514469393369"
 									autoLoad={false}
 									callback={this.responseFacebook}
 									render={renderProps => (
-										<Button variant="contained" color="primary"
-										        fullWidth
-										        className={classes.facebookButton}
-										        onClick={renderProps.onClick}
-										>
-											<FacebookIcon className={classes.leftIcon}/>
+										<Button
+											variant="contained"
+											color="primary"
+											fullWidth
+											className={classes.facebookButton}
+											onClick={renderProps.onClick}>
+											<FacebookIcon className={classes.leftIcon} />
 											Continue with Facebook
 										</Button>
 									)}
@@ -190,26 +205,23 @@ class Login extends Component {
 									onSuccess={this.responseGoogle}
 									onFailure={this.responseGoogle}
 									render={renderProps => (
-										<Button variant="contained" color="primary" fullWidth
-										        className={classes.googleButton}
-										        onClick={renderProps.onClick}
-										>
-											<GoogleIcon className={classes.leftIcon}/>
+										<Button
+											variant="contained"
+											color="primary"
+											fullWidth
+											className={classes.googleButton}
+											onClick={renderProps.onClick}>
+											<GoogleIcon className={classes.leftIcon} />
 											Sign in with Google
 										</Button>
-									)}/>
+									)}
+								/>
 
-								<Typography className={classes.addPaddingTop}>
-									Prefer not to log in?
-								</Typography>
+								<Typography className={classes.addPaddingTop}>Prefer not to log in?</Typography>
 
-								<Button href={'https://immutable.chordboard.co'}>
-									Go to the previous version
-								</Button>
-
+								<Button href={'https://immutable.chordboard.co'}>Go to the previous version</Button>
 							</Grid>
 						</Paper>
-
 					</form>
 				</Grid>
 			</div>
@@ -222,4 +234,9 @@ const mapDispatchToProps = {
 };
 //export default withRouter( connect( mapStateToProps, actions )( withStyles( styles )( Navbar ) ) );
 
-export default withRouter( connect( null, mapDispatchToProps )( withStyles( styles )( Login ) ) );
+export default withRouter(
+	connect(
+		null,
+		mapDispatchToProps
+	)(withStyles(styles)(Login))
+);
