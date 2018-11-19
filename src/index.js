1334b51c70989fdaae5d75b95beb3213e1146d01
Justin Lawrence <justinl@m2north.com>
2018-11-19 15:38:39 +0000

    add sentry

--- src/index.js
+++ src/index.js
@@ -13,6 +13,12 @@ import App from './App';
 
 import './styles/main.scss';
 
+import * as Sentry from '@sentry/browser';
+
+if ( process.env.NODE_ENV === 'production' ) {
+	Sentry.init({ dsn: 'https://2c14c1a4ae774cdd9c80545e7a34c2e7@sentry.io/1246393' });
+}
+
 // const noSleep = new NoSleep();
 const store = configureStore();
 
