a19ca39b098268d5560f5d5a74c150b9955579f3
Brett Henderson <bretth@m2north.com>
2019-05-21 18:01:45 +0200

    exclude unsupported devices from firestore persistence

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,17 @@
-/* global firebase */
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+import semver from 'semver'
+import {
+	osVersion,
+	osName,
+	mobileVendor,
+	browserName,
+	browserVersion,
+	engineName,
+	engineVersion,
+} from 'react-device-detect'
 
 // Initialize Firebase.
 firebase.initializeApp({
@@ -10,21 +23,30 @@ firebase.initializeApp({
 	messagingSenderId: '839278764423',
 })
 
-export const firestore = firebase.firestore()
-export const db = firestore
-// Disable deprecated features
-firestore.settings({ timestampsInSnapshots: true })
+if (process.env.NODE_ENV === 'development') {
+	window.firebase = firebase
+}
 
-firestore.enablePersistence().catch(err => {
-	if (err.code === 'failed-precondition') {
-		// Multiple tabs open, persistence can only be enabled
-		// in one tab at a a time.
-		// ...
-	} else if (err.code === 'unimplemented') {
-		// The current browser does not support all of the
-		// features required to enable persistence
-		// ...
-	} else {
-		console.error(err)
-	}
-})
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+// Deprecated
+export const db = firestore
+
+const unsupportedPersistence =
+	browserName === 'Mobile Safari' &&
+	semver.satisfies(semver.valid(semver.coerce(browserVersion)), '>=9.0.0')
+if (!unsupportedPersistence) {
+	firestore.enablePersistence().catch(err => {
+		if (err.code === 'failed-precondition') {
+			console.error(
+				'Multiple tabs open, persistence can only be enabled in one tab at a a time.'
+			)
+		} else if (err.code === 'unimplemented') {
+			console.error(
+				'The current browser does not support all of the features required to enable persistence'
+			)
+		} else {
+			console.error(err)
+		}
+	})
+}
