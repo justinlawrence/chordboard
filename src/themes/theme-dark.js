106cd9641a35271df0d70129830d5d404c219f2a
Justin Lawrence <justinl@m2north.com>
2019-06-21 18:01:28 +0100

    dark theme remove comments

--- src/themes/theme-dark.js
+++ src/themes/theme-dark.js
@@ -1,9 +1,9 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 
-const baseDarkTheme = createMuiTheme( {
+const baseDarkTheme = createMuiTheme({
 	palette: { type: 'dark' },
 	typography: { useNextVariants: true },
-} )
+})
 
 export const darkTheme = {
 	typography: {
@@ -13,7 +13,7 @@ export const darkTheme = {
 		...baseDarkTheme.palette,
 		background: {
 			...baseDarkTheme.palette.background,
-			hero: '#2a2a2a'
+			hero: '#2a2a2a',
 		},
 		primary: {
 			main: '#F73859', //fab
@@ -34,53 +34,3 @@ export const darkTheme = {
 	heroBackgroundColor: '#f0f4fc', //hero was #dbedf3m then f5f5f5
 	heroContrastText: '#000',
 }
-
-/*
-
-Based on this theme
-http://colorhunt.co/c/114174
-
-
-
-const defaultTheme = {
-	palette: {
-		primary: {
-			main: '#F73859', //fab
-			contrastText: '#fff'
-		},
-		secondary: {
-			main: '#283149', //nav was 283149 then fff
-			contrastText: '#fff' //was fff then 000
-		}
-	},
-
-	// Custom values
-	maxPageWidth: 1344,
-
-	heroBackgroundColor: '#dbedf3', //hero was #dbedf3m then f5f5f5
-	heroContrastText: '#000'
-};
-*/
-
-/*
-
-//based on http://colorhunt.co/c/118766
-
-const defaultTheme = {
-	palette: {
-		primary: {
-			main: '#EF7B7B', //fab
-			contrastText: '#fff'
-		},
-		secondary: {
-			main: '#C4EADA', //nav
-			contrastText: '#000'
-		}
-	},
-
-	// Custom values
-	maxPageWidth: 1344,
-	heroBackgroundColor: '#FCF3CA' //hero
-};
-
-*/
